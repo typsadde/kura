c28c9ada2bc02cb5a0af15214baf2ae2d634ace1
Maiero <matteo.maiero@eurotech.com>
2018-05-24 15:19:35 +0200

    New CloudConnection APIs drop, updated implementation and examples. Added Initial contribution for the Eclipse IoT cloud connection service.
    Signed-off-by: Maiero <matteo.maiero@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/wire/WireHelperService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/wire/WireHelperService.java
@@ -114,6 +114,7 @@ public interface WireHelperService {
      * @return the wire support instance
      * @throws NullPointerException
      *             if the argument is null
+     * @since 2.0
      */
     public WireSupport newWireSupport(WireComponent wireComponent, ServiceReference<WireComponent> wireComponentRef);
 
